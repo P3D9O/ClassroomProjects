b05ba9758b88523327e5f9853b37e6686c5d582e
P3D9O <110838357+P3D9O@users.noreply.github.com>
2024-10-08 19:28:28 -0400

    Add files via upload
    First main function

---
+++ main.c
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+Autor: Pedro e Maria
+Data: 28/08/2024
+Modulo: Logica de programação
+Curso: Eletronica 4.0
+Local: INDT
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main (void) 
+{
+    printf("Hello, Word2222\n");
+    return 0;
+
+}
